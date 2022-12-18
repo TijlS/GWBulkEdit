aabb17245cf32cde92954f1496825774a1aaf549
Tijl Schipper <tijl.schipper@gmail.com>
2022-12-18 17:18:08 +0100

    V4.1.0@beta2 --> +domain/orgChooser, +config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
-import { admin_directory_v1, google, GoogleApis } from "googleapis";
+import { admin_directory_v1, google } from "googleapis";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-import { writeFileSync as fsWriteFileSync } from "fs"
+import { writeFileSync as fsWriteFileSync } from "fs";
 import path from "path";
 import { cwd } from "process";
 import { OAuth2Client } from "google-auth-library";
@@ -17,6 +17,7 @@ import { saveSMSCUsersToJson } from "./functions/smartschoolHandler.js";
 import { startDbViewer } from "./functions/dbViewer.js";
 
 import config from "./config/config.json" assert { type: "json" };
+import { domainChooser, orgChooser } from "./helpers/domain_org_chooser.js";
 
 const whatQuestion = [
 	{
@@ -26,35 +27,35 @@ const whatQuestion = [
 			new inquirer.Separator("USERS"),
 			{
 				name: "Change users primary email",
-				value: 'change_primary_email'
+				value: "change_primary_email",
 			},
 			{
 				name: "Save users to local database",
-				value: 'save_to_db'
+				value: "save_to_db",
 			},
 			new inquirer.Separator("GROUPS"),
 			{
 				name: "Manage organization groups",
-				value: "manage_groups"
+				value: "manage_groups",
 			},
 			{
 				name: "Remove all users from groups",
-				value: 'empty_groups'
+				value: "empty_groups",
 			},
 			new inquirer.Separator("SMARTSCHOOL"),
 			{
 				name: "Save all SMSC users to JSON file",
-				value: 'smscs_all_to_json'
+				value: "smscs_all_to_json",
 			},
 			new inquirer.Separator("CONFIG"),
 			{
 				name: "Clear all files in generated directory",
-				value: 'clear_generated'
+				value: "clear_generated",
 			},
 			new inquirer.Separator("DATABASE"),
 			{
 				name: "View database online",
-				value: 'db_viewer'
+				value: "db_viewer",
 			},
 		],
 		name: "what",
@@ -78,76 +79,6 @@ const queryTypeQuestion = [
 		},
 	},
 ];
-const domainQueryQuestions = [
-	{
-		type: "input",
-		message: "Enter the domain name from wich you want to get the users",
-		name: "domainName",
-		/**
-		 *
-		 * @param {String} input
-		 */
-		validate: function (input) {
-			let done = this.async();
-
-			setTimeout(() => {
-				if (!input.includes(".")) {
-					done("Please provide valid domain");
-					return;
-				}
-				done(null, true);
-			}, 500);
-		},
-	},
-	{
-		type: "input",
-		message: "To wich domain do you want to transfer them?",
-		name: "newDomain",
-		/**
-		 *
-		 * @param {String} input
-		 */
-		validate: function (input) {
-			let done = this.async();
-
-			setTimeout(() => {
-				if (!input.includes(".")) {
-					done("Please provide valid domain");
-					return;
-				}
-				done(null, true);
-			}, 500);
-		},
-	},
-];
-const organisationQueryQuestions = [
-	{
-		type: "input",
-		message:
-			"Enter the organisation path from wich you want to get the users",
-		name: "organisationName",
-	},
-	{
-		type: "input",
-		message: "To wich domain do you want to transfer them?",
-		name: "newDomain",
-		/**
-		 *
-		 * @param {String} input
-		 */
-		validate: function (input) {
-			let done = this.async();
-
-			setTimeout(() => {
-				if (!input.includes(".")) {
-					done("Please provide valid domain");
-					return;
-				}
-				done(null, true);
-			}, 500);
-		},
-	},
-];
 
 const FLAGS = yargs(hideBin(process.argv))
 	.option("dev", {
@@ -163,10 +94,17 @@ const FLAGS = yargs(hideBin(process.argv))
 authorize().then(startProgram);
 
 /**
- * 
- * @param {OAuth2Client} auth 
+ *
+ * @param {OAuth2Client} auth
  */
 async function startProgram(auth) {
+	//Create Google Admin Service
+	const service = google.admin({
+		version: "directory_v1",
+		auth,
+		timeout: 1000,
+	});
+
 	if (config.is_first_time) {
 		console.log(`
 ${chalk.bgGreenBright("=== Welcome to GWBulkEdit ===")}
@@ -179,32 +117,59 @@ Before we start, some configuration has to be done.
 					name: "Enable SMSC integration",
 					value: "enable_smsc",
 				},
+				{
+					name: "Prefetch domains on launch",
+					value: "enable_domain_prefetch",
+				},
 			],
 			name: "configOptions",
 		});
-		if (config_options_answers.configOptions.includes("enable_smsc")) {
-			config.smsc_enabled = true;
+		for (const configOption of config_options_answers.configOptions) {
+			switch (configOption) {
+				case "enable_smsc":
+					config.smsc_enabled = true;
 
-			console.log(`
+					console.log(`
 ${chalk.yellow("=== SMSC configuration ===")}
 For using Smartschool, some extra configuration is needed.
 1. Activate the Smartschool API under
 	${chalk.italic("Algemene instellingen > Webservices")}
 	Preferably create a custom profile there
 2. Edit ${chalk.italic(
-				"config/smartschool.json"
-			)} and fill in the required fields.		
+						"config/smartschool.json"
+					)} and fill in the required fields.		
 			`);
-			const smsc_config_answers = await inquirer.prompt({
-				type: "confirm",
-				message: "Edited config/smartschool.json?",
-				name: "pass",
-			});
-			if(smsc_config_answers.pass !== true){
-				console.log(`
-	${chalk.redBright('Configuration failed')}
-	Try again!
-				`)
+					const smsc_config_answers = await inquirer.prompt({
+						type: "confirm",
+						message: "Edited config/smartschool.json?",
+						name: "pass",
+					});
+					if (smsc_config_answers.pass !== true) {
+						console.log(`
+${chalk.redBright("Configuration failed")}
+Try again!
+				`);
+						return process.exit(1);
+					}
+					break;
+
+				case "enable_domain_prefetch":
+					config.domain_prefetch_enabled = true;
+					console.log(`
+${chalk.yellow("=== Prefetching domains ===")}
+					`);
+					const domains = (
+						await service.domains.list({
+							customer: "my_customer",
+						})
+					).data.domains;
+					config.owned_domains = domains.map((domain) => {
+						return domain.domainName;
+					});
+					console.log(`
+${chalk.greenBright("Done!")}
+					`);
+					break;
 			}
 		}
 		console.log(`
@@ -215,28 +180,29 @@ The application will stop now, please restart it.
 		config.is_first_time = false;
 
 		//Rewrite config file
-		fsWriteFileSync(path.join(cwd(), 'config/config.json'), JSON.stringify(config, null, 4))
+		fsWriteFileSync(
+			path.join(cwd(), "config/config.json"),
+			JSON.stringify(config, null, 4)
+		);
 
 		return process.exit(0);
 	}
 
-	//Create Google Admin Service
-	const service = google.admin({
-		version: "datatransfer_v1",
-		auth,
-		timeout: 1000,
-	});
-
-	//Save owned domains to config
+	//Domain prefetching
 	// TODO: Needs testing!
-	if(!FLAGS.dev){
-		const domains = (await service.domains.list({
-			customer: 'my_customer'
-		})).data.domains
-		config.owned_domains = domains.map(domain => {
-			return domain.domainName
-		})
-		fsWriteFileSync(path.join(cwd(), 'config/config.json'), JSON.stringify(config, null, 4))
+	if (config.domain_prefetch_enabled) {
+		const domains = (
+			await service.domains.list({
+				customer: "my_customer",
+			})
+		).data.domains;
+		config.owned_domains = domains.map((domain) => {
+			return domain.domainName;
+		});
+		fsWriteFileSync(
+			path.join(cwd(), "config/config.json"),
+			JSON.stringify(config, null, 4)
+		);
 	}
 
 	return aksQuestions(service);
@@ -249,32 +215,29 @@ The application will stop now, please restart it.
  */
 async function aksQuestions(service) {
 	const what_question_answers = await inquirer.prompt(whatQuestion);
-	if (what_question_answers.what === 'change_primary_email') {
-		inquirer.prompt(queryTypeQuestion).then((answers) => {
-			if (answers.queryType == 1) {
-				inquirer.prompt(domainQueryQuestions).then((answers) => {
-					updateUsersPrimaryEmail(answers, service, 1, FLAGS);
-				});
-			} else if (answers.queryType == 2) {
-				inquirer.prompt(organisationQueryQuestions).then((answers) => {
-					updateUsersPrimaryEmail(answers, service, 2, FLAGS);
-				});
-			}
-		});
-	} else if (what_question_answers.what === 'manage_groups') {
+	if (what_question_answers.what === "change_primary_email") {
+		const query_type_answers = await inquirer.prompt(queryTypeQuestion);
+		if (query_type_answers.queryType == 1) {
+			const domain = await domainChooser();
+			updateUsersPrimaryEmail(domain, service, 1, FLAGS);
+		} else if (query_type_answers.queryType == 2) {
+			const orgPath = await orgChooser();
+			updateUsersPrimaryEmail(orgPath, service, 2, FLAGS);
+		}
+	} else if (what_question_answers.what === "manage_groups") {
 		console.warn(
 			chalk.white.bgYellow(" Warning! This feature is in development ")
 		);
 		process.exit(1);
-	} else if (what_question_answers.what === 'save_to_db') {
+	} else if (what_question_answers.what === "save_to_db") {
 		saveUsersToLocalFile(service, FLAGS);
-	} else if (what_question_answers.what === 'clear_generated') {
+	} else if (what_question_answers.what === "clear_generated") {
 		clearlocalFiles();
-	} else if (what_question_answers.what === 'empty_groups') {
+	} else if (what_question_answers.what === "empty_groups") {
 		removeGroups(service, FLAGS);
-	} else if (what_question_answers.what === 'smscs_all_to_json') {
+	} else if (what_question_answers.what === "smscs_all_to_json") {
 		saveSMSCUsersToJson();
-	} else if (what_question_answers.what === 'db_viewer') {
+	} else if (what_question_answers.what === "db_viewer") {
 		startDbViewer();
 	}
 }
