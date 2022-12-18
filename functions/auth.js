990b29f4613d2e947263319a91a26e473c0b65a2
Tijl Schipper <tijl.schipper@gmail.com>
2022-12-18 17:29:18 +0100

    V4.1.0@beta3 --> moved scopes to config file

--- functions/auth.js
+++ functions/auth.js
@@ -3,16 +3,8 @@ import { authenticate } from "@google-cloud/local-auth";
 import fs from "fs/promises";
 import path from "path";
 import { cwd } from "process";
+import SCOPES from "../config/scopes.json" assert { type: "json" }
 
-const SCOPES = [
-	"https://www.googleapis.com/auth/admin.directory.user",
-	"https://www.googleapis.com/auth/admin.directory.user.alias",
-	"https://www.googleapis.com/auth/admin.directory.group",
-	"https://www.googleapis.com/auth/admin.directory.group.member",
-	"https://www.googleapis.com/auth/admin.directory.orgunit",
-	"https://www.googleapis.com/auth/admin.directory.userschema",
-	"https://www.googleapis.com/auth/admin.directory.domain.readonly"
-];
 const TOKEN_PATH = path.join(cwd(), "config/token.json");
 const CREDENTIALS_PATH = path.join(cwd(), "config/credentials.json");
 
